238b27bf80b985fb302e98f79e1be526ce208202
yarski25 <yaroslav.saprykin@gmail.com>
2023-08-01 18:49:45 +0200

    weather service implemented

--- yboard/src/components/card/MyCard.tsx
+++ yboard/src/components/card/MyCard.tsx
@@ -1,63 +1,109 @@
 import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
-import { Box, Card, CardActions, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
 import { Coords } from "../../types/Coords";
 import WeatherService from "../../api/WeatherService";
 import { useFetching } from "../../hooks/useFetching";
 
+type Image = {
+  src: string;
+  alt: string;
+};
+
+type Location = {
+  name: string;
+  region: string;
+  country: string;
+};
+
+type Wind = {
+  wind_kph: string;
+  wind_ms: string;
+  wind_degree: string;
+  wind_dir: string;
+};
+
 const MyCard = () => {
-  const [location, setLocation] = useState<Coords>({ lat: "", lon: "" });
+  const [position, setPosition] = useState<Coords>({ lat: "", lon: "" });
+  const [location, setLocation] = useState<Location>({
+    name: "",
+    region: "",
+    country: "",
+  });
+  const [wind, setWind] = useState<Wind>({
+    wind_kph: "",
+    wind_ms: "",
+    wind_degree: "",
+    wind_dir: "",
+  });
+  const [temperature, setTemperature] = useState<string>("");
+  const [image, setImage] = useState<Image>({ src: "", alt: "" });
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
 
-  let temperature = "";
+  useEffect(() => {
+    console.log("useEffect called");
+    WeatherService.getByCoords({ lat: position.lat, lon: position.lon })
+      .then((response) => {
+        setIsLoading(true);
+        setTemperature(response.data.current.temp_c.toString());
+        setImage({
+          src: "https:" + response.data.current.condition.icon,
+          alt: response.data.current.condition.text,
+        });
+        setLocation({
+          name: response.data.location.name,
+          region: response.data.location.region,
+          country: response.data.location.country,
+        });
+        setWind({
+          wind_kph: response.data.current.wind_kph.toFixed(0).toString(),
+          wind_ms: ((response.data.current.wind_kph * 1000) / 3600)
+            .toFixed(0)
+            .toString(),
+          wind_degree: response.data.current.wind_degree.toString(),
+          wind_dir: response.data.current.wind_dir,
+        });
+      })
+      .catch((e) => {
+        setError((e as Error).message);
+      })
+      .finally(() => setIsLoading(false));
+  }, [position]);
 
-  useEffect(
-    () => console.log("re-render because location changed:", temperature),
-    [temperature]
-  );
-
-  function handleOnClick() {
+  const handleOnClick = () => {
     if ("geolocation" in navigator) {
       console.log("Available");
       navigator.geolocation.getCurrentPosition(
-        function (position) {
-          const latitude = position.coords.latitude.toString();
-          const longitude = position.coords.longitude.toString();
-          setLocation({
-            lat: latitude,
-            lon: longitude,
+        (position) => {
+          setPosition({
+            lat: position.coords.latitude.toFixed(6).toString(),
+            lon: position.coords.longitude.toFixed(6).toString(),
           });
-          //console.log("Latitude is :", location.lat);
-          //console.log("Longitude is :", location.lon);
-          console.log("Latitude is :", position.coords.latitude);
-          console.log("Longitude is :", position.coords.longitude);
-          WeatherService.getByCoords({ lat: latitude, lon: longitude })
-            .then((response) => {
-              setIsLoading(true);
-              temperature = response.data.current.temp_c.toString();
-              console.log(temperature);
-            })
-            .catch((e) => {
-              setError((e as Error).message);
-            })
-            .finally(() => setIsLoading(false));
         },
-        function (error) {
+        (error) => {
           console.error("Error Code = " + error.code + " - " + error.message);
         }
       );
     } else {
       console.log("Not Available");
     }
-  }
+  };
 
   return (
     <div>
       <Button onClick={handleOnClick} variant="contained" color="secondary">
         Get location
       </Button>
-      <Box sx={{ minWidth: 275 }}>
+      <Box sx={{ maxWidth: 150 }}>
         <Card
           variant="outlined"
           sx={{ border: "2px solid purple", borderRadius: "16px" }}
@@ -68,13 +114,29 @@ const MyCard = () => {
               color="text.secondary"
               gutterBottom
             >
-              Word of the Day
+              {location.country}
             </Typography>
             <Typography variant="h5" component="div">
-              {temperature}
+              {location.name}
+            </Typography>
+            <CardMedia
+              component="img"
+              height="100"
+              image={image.src}
+              alt={image.alt}
+            />
+            {!isLoading ? (
+              <Typography variant="h5" component="div">
+                {temperature}℃
+              </Typography>
+            ) : (
+              <CircularProgress color="secondary" />
+            )}
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {wind.wind_kph} km/h {wind.wind_dir}
             </Typography>
             <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
+              {wind.wind_ms} m/s
             </Typography>
             <Typography variant="body2">
               well meaning and kindly.
--- yboard/src/types/interfaces/Current.ts
+++ yboard/src/types/interfaces/Current.ts
@@ -1,6 +1,6 @@
 import { Location } from "./Location";
 
-interface Conditions {
+interface Condition {
   text: string;
   icon: string;
   code: number;
@@ -11,7 +11,22 @@ interface WeatherData {
   temp_c: number;
   temp_f: number;
   is_day: number;
-  conditions: Conditions;
+  condition: Condition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  vis_km: number;
+  vis_miles: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
 }
 
 export interface Current {
