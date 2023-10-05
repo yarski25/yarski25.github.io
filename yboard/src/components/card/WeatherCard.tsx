136b0f141e1e193f2a98b02e3270614aa69b5de7
yarski25 <yaroslav.saprykin@gmail.com>
2023-10-05 12:16:33 +0200

    icons integrated

--- yboard/src/components/card/WeatherCard.tsx
+++ yboard/src/components/card/WeatherCard.tsx
@@ -8,6 +8,7 @@ import {
   Collapse,
   IconButton,
   IconButtonProps,
+  Stack,
   Typography,
   styled,
 } from "@mui/material";
@@ -25,6 +26,10 @@ import {
 } from "../../styles/card";
 import { ExpandMore } from "../ui/buttons/ExpandMore";
 import humidity from "../../assets/humidity.webp";
+import wind from "../../assets/wind.webp";
+import aqi from "../../assets/air-quality.webp";
+import temp from "../../assets/temp.webp";
+import Item from "../ui/Item/Item";
 
 type WeatherCardProps = {
   index: number;
@@ -70,7 +75,7 @@ const WeatherCard = ({
                 : weatherData?.current?.condition?.text
             }
           />
-          <Typography
+          {/* <Typography
             variant="h5"
             component="div"
             sx={{ mb: 1.5, fontSize: "1.5em" }}
@@ -82,8 +87,17 @@ const WeatherCard = ({
                   .toString()
               : Number(weatherData?.current?.temp_c).toFixed(0).toString()}
             ℃
-          </Typography>
-          <Typography
+          </Typography> */}
+
+          <Item src={temp} alt="temperature">
+            {day > 0
+              ? Number(weatherData?.forecast?.forecastday?.[day].day?.maxtemp_c)
+                  .toFixed(0)
+                  .toString()
+              : Number(weatherData?.current?.temp_c).toFixed(0).toString()}
+            ℃
+          </Item>
+          {/* <Typography
             sx={{ mb: 1.5, fontSize: "0.5em" }}
             color="text.secondary"
           >
@@ -100,20 +114,99 @@ const WeatherCard = ({
             {day > 0
               ? weatherData?.forecast?.forecastday?.[day].hour?.[hour].wind_dir
               : weatherData?.current?.wind_dir}
-          </Typography>
-
-          {/* <img src={humidity} width={16} height={16} alt="humidity" /> */}
-          <Typography
-            sx={{ mb: 1.5, fontSize: "0.5em" }}
-            color="text.secondary"
+          </Typography> */}
+          <Item src={wind} alt="wind">
+            {day > 0
+              ? Number(
+                  weatherData?.forecast?.forecastday?.[day].day?.maxwind_kph
+                )
+                  .toFixed(0)
+                  .toString()
+              : Number(weatherData?.current?.wind_kph)
+                  .toFixed(0)
+                  .toString()}{" "}
+            km/h{" "}
+            {day > 0
+              ? weatherData?.forecast?.forecastday?.[day].hour?.[hour].wind_dir
+              : weatherData?.current?.wind_dir}
+          </Item>
+          {/* <div className="weather-card-item">
+            <div className="weather-card-item__icon">
+              <img src={humidity} width={24} height={24} alt="humidity" />
+            </div>
+            <div className="weather-card-item__text">
+              <Typography
+                sx={{
+                  mb: 1.5,
+                  fontSize: "1em",
+                  textAlign: "center",
+                  alignItems: "center",
+                }}
+                color="text.secondary"
+              >
+                {" "}
+                {day > 0
+                  ? weatherData?.forecast?.forecastday?.[day].day?.avghumidity
+                  : weatherData?.current?.humidity}{" "}
+                %
+              </Typography>
+            </div>
+          </div> */}
+          {/* <Stack
+            spacing={0.25}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            // sx={{
+            //   display: "flex",
+            //   flexDirection: "row",
+            //   justifyContent: "center",
+            //   alignItems: "center",
+            // }}
           >
-            humidity:{" "}
+            <img
+              src={humidity}
+              width={16}
+              height={16}
+              alt="humidity"
+              style={{ marginRight: "0.5em" }}
+            />
+            <Typography
+              sx={{
+                mb: 0,
+                fontSize: "1em",
+                textAlign: "center",
+                alignItems: "center",
+              }}
+              color="text.secondary"
+            >
+              {" "}
+              {day > 0
+                ? weatherData?.forecast?.forecastday?.[day].day?.avghumidity
+                : weatherData?.current?.humidity}{" "}
+              %
+            </Typography>
+          </Stack> */}
+
+          <Item src={humidity} alt="humidity">
             {day > 0
               ? weatherData?.forecast?.forecastday?.[day].day?.avghumidity
               : weatherData?.current?.humidity}{" "}
             %
-          </Typography>
-          <Typography
+          </Item>
+
+          <Item src={aqi} alt="air quality">
+            {day > 0 &&
+            weatherData?.forecast?.forecastday?.[day].day?.air_quality?.[
+              "us-epa-index"
+            ]
+              ? weatherData?.forecast?.forecastday?.[day].day?.air_quality?.[
+                  "us-epa-index"
+                ]
+              : weatherData?.current?.air_quality?.["us-epa-index"]}
+          </Item>
+
+          {/* <Typography
             sx={{ mb: 1.5, fontSize: "0.5em" }}
             color="text.secondary"
           >
@@ -126,7 +219,7 @@ const WeatherCard = ({
                   "us-epa-index"
                 ]
               : weatherData?.current?.air_quality?.["us-epa-index"]}
-          </Typography>
+          </Typography> */}
         </StyledCardContent>
         <CardActions>
           <ExpandMore
---
+++ yboard/src/components/ui/Item/Item.tsx
@@ -0,0 +1,41 @@
+import React, { PropsWithChildren } from "react";
+import { Stack, Typography } from "@mui/material";
+
+type ItemProps = {
+  src?: string; // icon source
+  alt?: string; // icon alt text
+  iconSize?: string; // icon size
+  style?: React.CSSProperties;
+};
+
+const Item = (props: PropsWithChildren<ItemProps>) => {
+  return (
+    <Stack
+      spacing={0.25}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <img
+        src={props.src}
+        width={props.iconSize || 16}
+        height={props.iconSize || 16}
+        alt={props.alt || "no icon"}
+        style={{ marginRight: "0.5em" }}
+      />
+      <Typography
+        sx={{
+          mb: 0,
+          fontSize: "1em",
+          textAlign: "center",
+          alignItems: "center",
+        }}
+        color="text.secondary"
+      >
+        {props.children}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default Item;
